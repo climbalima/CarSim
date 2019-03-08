c5fccfba5d1701cdb905b1dd5cc28fdf8c6a9ef7
Eliza Doering <elizadoering@gmail.com>
2019-03-08 12:52:11 -0500

    location updated (again)

--- src/racesim/Location.java
+++ src/racesim/Location.java
@@ -3,68 +3,101 @@ package racesim;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
+import javafx.scene.shape.Circle;
 
 public class Location {
     
-    private String name;
-    private int time;
-    private double[][] location;
+    private ArrayList<Car> curCars;
+    private ArrayList<Car> incomingCars;
+    private char LocationID;
+    private double x_coord;
+    private double y_coord;
+    private Circle locationVisual;
 
-    public Location(String name, int time, double[][] location) {
-        this.name = name;
-        this.time = time;
-        this.location = location;
+    public Location(char LocationID, double x_coord, double y_coord, Circle locationVisual) {
+        this.LocationID = LocationID;
+        this.x_coord = x_coord;
+        this.y_coord = y_coord;
+        this.locationVisual = locationVisual;
     }
 
-    public Location() {}
-
-    public String getName() {
-        return name;
+    public ArrayList<Car> getCurCars() {
+        return curCars;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCurCars(ArrayList<Car> curCars) {
+        this.curCars = curCars;
     }
 
-    public int getTime() {
-        return time;
+    public ArrayList<Car> getIncomingCars() {
+        return incomingCars;
     }
 
-    public void setTime(int time) {
-        this.time = time;
+    public void setIncomingCars(ArrayList<Car> incomingCars) {
+        this.incomingCars = incomingCars;
     }
 
-    public double[][] getLocation() {
-        return location;
+    public char getLocationID() {
+        return LocationID;
     }
 
-    public void setLocation(double[][] location) {
-        this.location = location;
+    public void setLocationID(char LocationID) {
+        this.LocationID = LocationID;
+    }
+
+    public double getX_coord() {
+        return x_coord;
+    }
+
+    public void setX_coord(double x_coord) {
+        this.x_coord = x_coord;
+    }
+
+    public double getY_coord() {
+        return y_coord;
+    }
+
+    public void setY_coord(double y_coord) {
+        this.y_coord = y_coord;
+    }
+
+    public Circle getLocationVisual() {
+        return locationVisual;
+    }
+
+    public void setLocationVisual(Circle locationVisual) {
+        this.locationVisual = locationVisual;
     }
     
-    public void addedTime(){
+    public void addCurCar(Car car){
         
     }
     
-    public void addCar(){
+    public void addIncomingCar(Car car){
         
     }
     
-    public ArrayList currentCars(){
+    public void removeIncomingCar(Car car){
         
     }
-
+    
+    public void removeCurCar(Car car){
+        
+    }
     @Override
     public String toString() {
-        return "Location{" + "name=" + name + ", time=" + time + ", location=" + location + '}';
+        return "Location{" + "curCars=" + curCars + ", incomingCars=" + incomingCars + ", LocationID=" + LocationID + ", x_coord=" + x_coord + ", y_coord=" + y_coord + ", locationVisual=" + locationVisual + '}';
     }
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 59 * hash + Objects.hashCode(this.name);
-        hash = 59 * hash + this.time;
-        hash = 59 * hash + Arrays.deepHashCode(this.location);
+        int hash = 3;
+        hash = 43 * hash + Objects.hashCode(this.curCars);
+        hash = 43 * hash + Objects.hashCode(this.incomingCars);
+        hash = 43 * hash + this.LocationID;
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.x_coord) ^ (Double.doubleToLongBits(this.x_coord) >>> 32));
+        hash = 43 * hash + (int) (Double.doubleToLongBits(this.y_coord) ^ (Double.doubleToLongBits(this.y_coord) >>> 32));
+        hash = 43 * hash + Objects.hashCode(this.locationVisual);
         return hash;
     }
 
@@ -80,7 +113,28 @@ public class Location {
             return false;
         }
         final Location other = (Location) obj;
+        if (this.LocationID != other.LocationID) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.x_coord) != Double.doubleToLongBits(other.x_coord)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.y_coord) != Double.doubleToLongBits(other.y_coord)) {
+            return false;
+        }
+        if (!Objects.equals(this.curCars, other.curCars)) {
+            return false;
+        }
+        if (!Objects.equals(this.incomingCars, other.incomingCars)) {
+            return false;
+        }
+        if (!Objects.equals(this.locationVisual, other.locationVisual)) {
+            return false;
+        }
         return true;
     }
+    
+
+
 
 }
