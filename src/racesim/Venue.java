ed20c7f72fc9164eb49084c7412ad135674ad246
climbalima <climbalima@gmail.com>
2019-03-12 16:09:14 -0400

    started venue

--- src/racesim/Venue.java
+++ src/racesim/Venue.java
@@ -2,8 +2,48 @@ package racesim;
 
 import javafx.scene.shape.Rectangle;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Venue {
     private Rectangle venueVisual;
-    private HashMap<String, HashMap<Integer, Integer>> map;
+    private HashMap<String, HashMap<Integer, Integer>> cords;
+    private ArrayList<Car> cars;
+
+    public Venue(Rectangle venueVisual, HashMap<String, HashMap<Integer, Integer>> cords, ArrayList<Car> cars) {
+        this.venueVisual = venueVisual;
+        this.cords = cords;
+        this.cars = cars;
+    }
+    
+    public Venue(){
+        
+    }
+
+    public Rectangle getVenueVisual() {
+        return venueVisual;
+    }
+
+    public HashMap<String, HashMap<Integer, Integer>> getCords() {
+        return cords;
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void setVenueVisual(Rectangle venueVisual) {
+        this.venueVisual = venueVisual;
+    }
+
+    public void setCords(HashMap<String, HashMap<Integer, Integer>> cords) {
+        this.cords = cords;
+    }
+
+    public void setCars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+    
+    
+    
+    
 }
