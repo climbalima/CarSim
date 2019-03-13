51bd718d8491cb5be181ecdc2e5c519be2047284
Eliza Doering <elizadoering@gmail.com>
2019-03-13 13:29:01 -0400

    RaceSim update

--- src/racesim/RaceSim.java
+++ src/racesim/RaceSim.java
@@ -6,6 +6,7 @@
 package racesim;
 
 import java.util.ArrayList;
+import java.util.Random;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,8 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 /**
@@ -23,26 +23,29 @@ import javafx.scene.paint.Color;
  */
 public class RaceSim extends Application {
     private ArrayList<Color> colors;
+    private ArrayList<Car> cars;
+    private ArrayList<Location> locations;
+    private CarData carData;
+    private int length,width;
+    private BorderPane bp;
+    private Button start;
+    private Button reset;
+    
+    public RaceSim(){
+        colors = new ArrayList<Color>();
+        cars = new ArrayList<Car>();
+        locations = new ArrayList<Location>();
+        carData=new CarData(cars,locations);
+        bp=new BorderPane();
+        start=new Button("Start");
+        reset=new Button("Reset");
+    }
     @Override
     public void start(Stage primaryStage) {
-        BorderPane layout = new BorderPane();
-        HBox buttons = new HBox();
-        VBox data = new VBox();
-        Venue course = new Venue();
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        BorderPane root = new BorderPane();
-        root.setBottom(buttons);
+        Pane root = new Pane();
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 1000, 800);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
@@ -55,18 +58,52 @@ public class RaceSim extends Application {
         //call in for loop in start method
         //set line color in switch statement
     }
+    public void startButton(){
+        
+    }
     public void buildVenue(){
         
     }
     public void buildLocations(){
+        double xUpper= 690;double yUpper=590;
+        double loc1x = Math.random()*xUpper;
+        double loc1y = Math.random()*yUpper;
+        locations.set(0, new Location('A',loc1x,loc1y));
+        double loc2x = Math.random()*xUpper;
+        double loc2y = Math.random()*yUpper;
+        if(loc2y==loc1y||loc2x==loc1x){
+            loc2x = Math.random()*xUpper;
+            loc2y = Math.random()*yUpper;
+        }
+        locations.set(1,new Location('B',loc2x,loc2y));
+        double loc3x = Math.random()*xUpper;
+        double loc3y = Math.random()*yUpper;
+        if(loc3y==loc2y||loc3y==loc1y||loc3x==loc2x||loc3x==loc1x){
+            loc3x = Math.random()*xUpper;
+            loc3y = Math.random()*yUpper;
+        }
+        locations.set(2,new Location('C',loc3x,loc3y));
+        double loc4x = Math.random()*xUpper;
+        double loc4y = Math.random()*yUpper;
+        if(loc4y==loc3y||loc4y==loc2y||loc4y==loc1y||loc4x==loc3x||loc4x==loc2x||loc4x==loc1x){
+            loc4y = Math.random()*xUpper;
+            loc4x = Math.random()*yUpper;
+        }
+        locations.set(3,new Location('D',loc4x,loc4y));
+    }
+    public void buildCarData(){
         
     }
-    public void carVisuals(){
+    public void animateCars(){
     
     }
     public void checkOver(){
         
     }
+    public void reset(){
+        
+    }
+
     
 
     /**
