5e8895b1df7ca1d1957f51d4cce916f0e06a129c
climbalima <climbalima@gmail.com>
2019-03-13 13:35:30 -0400

    Merge origin/master
    Conflicts:
    	src/racesim/RaceSim.java
    	src/racesim/Venue.java
    
    added images method

--- src/racesim/RaceSim.java
+++ src/racesim/RaceSim.java
@@ -16,6 +16,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
 /**
  *
