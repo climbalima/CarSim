f2c7b3e52ac0e7b7ee9f5e7e804aa47c97675f9e
Miguel Oyler-Castrillo <33669627+migueloyler@users.noreply.github.com>
2019-03-16 14:37:03 -0400

    Update RaceSim.java

--- src/racesim/RaceSim.java
+++ src/racesim/RaceSim.java
@@ -5,6 +5,7 @@
  */
 package racesim;
 
+
 import java.util.ArrayList;
 import java.util.Random;
 import javafx.application.Application;
@@ -18,6 +19,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import java.util.HashMap;
+
 /**
  *
  * @author max
@@ -25,8 +27,7 @@ import java.util.HashMap;
 public class RaceSim extends Application {
     private ArrayList<Color> colors;
     private ArrayList<Car> cars;
-    private ArrayList<Location> locations;
-    private ArrayList<Image> carPics;
+    private HashMap<Character, Location> locations;
     private CarData carData;
     private Venue venue;
     private int length,width;
@@ -36,10 +37,9 @@ public class RaceSim extends Application {
     public RaceSim(){
         colors = new ArrayList<Color>();
         cars = new ArrayList<Car>();
-        locations = new ArrayList<Location>();
-        carPics = new ArrayList<Image>();
+        locations = new HashMap<Character, Location>();
         carData=new CarData(cars,locations,700,200);
-        venue = new Venue(locations,cars,700,800);
+        venue = new Venue(locations ,cars,700,800);
         start=new Button("Start");
         reset=new Button("Reset");
     }
@@ -55,7 +55,7 @@ public class RaceSim extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    public Car buildCar(int carID, Color color, Image carVisual){
+    public Car buildCar(int i, int carID, String color, Image carVisual){
         //create the cars
         //randomize the speed variable
         //call in for loop in buildVenue
@@ -68,91 +68,88 @@ public class RaceSim extends Application {
         
     }
     public void buildVenue(){
-        buildLocations();
-        for(int n = 0; n<4; n++){
-            cars.add(buildCar(n, colors.get(n), carPics.get(n)));
-        }
+        
     }
     public void buildLocations(){
         double xUpper= 690;double yUpper=590;
         double loc1x = Math.random()*xUpper;
         double loc1y = Math.random()*yUpper;
-        locations.set(0, new Location('A',loc1x,loc1y));
+        locations.put('a', new Location('a',loc1x,loc1y));
         double loc2x = Math.random()*xUpper;
         double loc2y = Math.random()*yUpper;
         if(loc2y==loc1y||loc2x==loc1x){
             loc2x = Math.random()*xUpper;
             loc2y = Math.random()*yUpper;
         }
-        locations.set(1,new Location('B',loc2x,loc2y));
+        locations.put('b',new Location('b',loc2x,loc2y));
         double loc3x = Math.random()*xUpper;
         double loc3y = Math.random()*yUpper;
         if(loc3y==loc2y||loc3y==loc1y||loc3x==loc2x||loc3x==loc1x){
             loc3x = Math.random()*xUpper;
             loc3y = Math.random()*yUpper;
         }
-        locations.set(2,new Location('C',loc3x,loc3y));
+        locations.put('c',new Location('c',loc3x,loc3y));
         double loc4x = Math.random()*xUpper;
         double loc4y = Math.random()*yUpper;
         if(loc4y==loc3y||loc4y==loc2y||loc4y==loc1y||loc4x==loc3x||loc4x==loc2x||loc4x==loc1x){
             loc4y = Math.random()*xUpper;
             loc4x = Math.random()*yUpper;
-        }
-        locations.set(3,new Location('D',loc4x,loc4y));
+        } 
+        locations.put('d',new Location('d',loc4x,loc4y));
     }
 
-    public void buildCarData(){
-        
-    }
-    
     public void generatePaths(ArrayList<Car> Cars, HashMap<Character, Location> locationMap){
+        String[] ad = new String[2];
+        String[] cb = new String[2];
+        String[] da = new String[2];
+        String[] bc = new String[2];
+        ad[0] = "abcd";
+        ad[1] = "acbd";
+        bc[0] = "badc";
+        bc[1] = "bdac";
+        cb[0] = "cadb";
+        cb[1] = "cdab";
+        da[0] = "dbca";
+        da[1] = "dcba";
         Random gen = new Random();
-        String paths = "abcd";
-
+        String path = "";
+        
         for (int i = 0; i < 4; i++) {
-            char[] path = new char[4];
-            path[0] = paths.charAt(i);
-            while(true) {
-                int randInt = gen.nextInt(4);
-                if (pathGenHelper(paths.charAt(i), paths.charAt(randInt))) {
-                    path[3] = paths.charAt(randInt);
+            int randInt = gen.nextInt(2);
+            switch(i){
+                case 0:
+                    path = ad[randInt];
                     break;
-                }
-
+                case 1:
+                    path = bc[randInt];
+                    break;
+                case 2:
+                    path = cb[randInt];
+                    break;
+                case 3:
+                    path = da[randInt];
+                    break;
             }
-            pathGenHelper2(path);
-            Location[] locationArr = charToLocationArray(path, locationMap);
+            Location[] locationArr = stringToLocationArray(path, locationMap);
             Cars.get(i).setPath(locationArr);
         }
     }
     
-    public Location[] charToLocationArray(char[] paths, HashMap<Character, Location> locationMap) {
+    public Location[] stringToLocationArray(String path, HashMap<Character, Location> locationMap) {
         Location[] locationArr = new Location[4];
         for (int i = 0; i < 4; i++) {
-            locationArr[i] = locationMap.get(paths[i]);
+            locationArr[i] = locationMap.get(path.charAt(i));
         }
         return locationArr;
     }
-    
-    public boolean pathGenHelper(char src, char dest) {
-        return src != dest;
-    }
-    
-    public void pathGenHelper2(char[] path) {
-        String paths = "abcd";
-        String newPath = paths.substring(0, paths.charAt(path[0])) + 
-        paths.substring(paths.charAt(path[0]) + 1);
-        String finalPath = paths.substring(0, paths.charAt(path[3])) + 
-        paths.substring(paths.charAt(path[3]) + 1);
-        path[1] = finalPath.charAt(0);
-        path[2] = finalPath.charAt(1);
-        
-    }
-    public void setCarVisuals(){
+
+    public ArrayList<Image> carVisuals(){
+        ArrayList<Image> carPics = new ArrayList<>();
         carPics.add(new Image("Cars-Lightning-McQueen-128.PNG"));
         carPics.add(new Image("Cars-Flo-128.PNG"));
         carPics.add(new Image("Cars-Mater-128.PNG"));
         carPics.add(new Image("Cars-Ramone-128.PNG"));
+        return carPics;
     }
     public boolean checkOver(ArrayList<Car> cars){
         for(Car c: cars){
